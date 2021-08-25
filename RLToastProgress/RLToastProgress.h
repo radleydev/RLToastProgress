d12149b466b48548234a90b970b5c5851bdc6a72
radleydev <radley.dev@gmail.com>
2021-08-25 09:39:34 +0700

    Initial a project

---
+++ RLToastProgress/RLToastProgress.h
@@ -0,0 +1,18 @@
+//
+//  RLToastProgress.h
+//  RLToastProgress
+//
+//  Created by htkien.dev@gmail.com on 25/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RLToastProgress.
+FOUNDATION_EXPORT double RLToastProgressVersionNumber;
+
+//! Project version string for RLToastProgress.
+FOUNDATION_EXPORT const unsigned char RLToastProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RLToastProgress/PublicHeader.h>
+
+
