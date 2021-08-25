0ab476ee4973795feb153eb279bafaa04283bbe0
radleydev <radley.dev@gmail.com>
2021-08-25 09:52:19 +0700

    Add test case

---
+++ RLToastProgress/RLToastProgress.swift
@@ -0,0 +1,21 @@
+//
+//  RLToastProgress.swift
+//  RLToastProgress
+//
+//  Created by Radley Hoang on 25/08/2021.
+//
+
+import Foundation
+
+public final class RLToastProgress {
+
+    let name = "RLToastProgress"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- RLToastProgressTests/RLToastProgressTests.swift
+++ RLToastProgressTests/RLToastProgressTests.swift
@@ -10,24 +10,17 @@ import XCTest
 
 class RLToastProgressTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var swiftyLib: RLToastProgress!
+    
+    override func setUp() {
+        swiftyLib = RLToastProgress()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSub() {
+        XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
